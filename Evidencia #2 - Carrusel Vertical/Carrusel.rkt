43057ed2e3e69bcb34f309c1829b7e9a3901e12c
Diego Figueroa <a01660987@tec.mx>
2023-05-05 19:07:02 -0600

    Agregar carrusel

--- Evidencia #2 - Carrusel Vertical/Carrusel.rkt
+++ Evidencia #2 - Carrusel Vertical/Carrusel.rkt
@@ -1,1 +1,15 @@
-(define )
+#lang racket
+
+(define carrusel (read (open-input-file "carrusel.txt")))
+(define filas (length carrusel))
+(define columnas (length (car carrusel)))
+
+carrusel
+filas
+columnas
+
+(define (coordenadas lst)
+  (apply append (map (lambda (n) ()) lst)))
+
+carrusel
+(coordenadas carrusel)
