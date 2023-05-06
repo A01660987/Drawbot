be356bc637c35769e04edce608fdac213a5e78e6
Diego Figueroa Peart <a01660987@tec.mx>
2023-05-05 21:43:20 -0600

    Generar lista de productos con coordenadas

--- Evidencia #2 - Carrusel Vertical/Carrusel.rkt
+++ Evidencia #2 - Carrusel Vertical/Carrusel.rkt
@@ -8,8 +8,13 @@ carrusel
 filas
 columnas
 
-(define (coordenadas lst)
-  (apply append (map (lambda (n) ()) lst)))
+(define (coordenadas lst f x y)
+  (if (null? lst) f
+      (if (<= y columnas)
+          (coordenadas (cdr lst) (append f (list (list (caar lst) (cons x y)))) x (+ y 1))
+          (coordenadas lst f (+ x 1) 1))))
 
-carrusel
-(coordenadas carrusel)
+(define diccionario (coordenadas (apply append carrusel) '() 1 1))
+
+(define (retirar cantidad producto)
+  ())
