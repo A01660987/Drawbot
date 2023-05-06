9594eaa959126ca5ffb3d211479713ef3e2d6e73
Diego Figueroa <a01660987@tec.mx>
2023-05-06 01:19:24 -0600

    Mostrar valor total y productos bajos/nulos

--- Evidencia #2 - Carrusel Vertical/Carrusel.rkt
+++ Evidencia #2 - Carrusel Vertical/Carrusel.rkt
@@ -1,12 +1,12 @@
 #lang racket
 
-(define carrusel (read (open-input-file "carrusel.txt")))
+(define archivo (open-input-file "carrusel.txt"))
+(define transacciones (open-input-file "transacciones.txt"))
+
+(define carrusel (read archivo))
+(define est-actual (read archivo))
 (define filas (length carrusel))
 (define columnas (length (car carrusel)))
-
-carrusel
-filas
-columnas
 
 (define (coordenadas lst f x y)
   (if (null? lst) f
@@ -16,5 +16,27 @@ columnas
 
 (define diccionario (coordenadas (apply append carrusel) '() 1 1))
 
-(define (retirar cantidad producto)
-  ())
+(define (buscar producto)
+  (cadar (filter (lambda (producto) (equal? producto (car diccionario))) diccionario)))
+
+(define valor-total (apply + (map (lambda (x) (* (cadr x) (caddr x))) (apply append carrusel))))
+
+(define (productos-bajos-aux lst f)
+  (if (null? lst) f
+      (productos-bajos-aux (cdr lst) (append f (list (list (caar lst) (cadar lst) (buscar (caar lst))))))))
+(define productos-bajos (productos-bajos-aux (filter (lambda (x) (> 20 (cadr x))) (apply append carrusel)) '()))
+
+(define a (print "hola"))
+
+(define (realizar-transacciones)
+  (define transaccion (read transacciones))
+  (if (eof-object? transaccion)
+      (void)
+      (begin
+        transaccion
+        (realizar-transacciones))))
+
+(realizar-transacciones)
+
+"Valor total del inventario:" valor-total
+"Productos con poco o nulo inventario:" productos-bajos
