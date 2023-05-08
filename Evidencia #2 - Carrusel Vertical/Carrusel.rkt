4044f092a54e0e30126b6eaba36b89a55f5e2f7d
Diego Figueroa <a01660987@tec.mx>
2023-05-08 04:26:33 -0600

    Agregar funciones

--- Evidencia #2 - Carrusel Vertical/Carrusel.rkt
+++ Evidencia #2 - Carrusel Vertical/Carrusel.rkt
@@ -1,12 +1,17 @@
-#lang racket
+#lang racket/load
 
-(define archivo (open-input-file "carrusel.txt"))
-(define transacciones (open-input-file "transacciones.txt"))
+(define transacciones (read (open-input-file "transacciones.txt")))
 
-(define carrusel (read archivo))
-(define est-actual (read archivo))
-(define filas (length carrusel))
-(define columnas (length (car carrusel)))
+(define (archivo)
+  (define lectura (open-input-file "carrusel.txt"))
+  (define contenido (read lectura))
+  (close-input-port lectura)
+  contenido)
+
+(define (carrusel) (car (archivo)))
+(define (est-actual) (cadr (archivo)))
+(define filas (length (carrusel)))
+(define columnas (length (car (carrusel))))
 
 (define (coordenadas lst f x y)
   (if (null? lst) f
@@ -14,29 +19,101 @@
           (coordenadas (cdr lst) (append f (list (list (caar lst) (cons x y)))) x (+ y 1))
           (coordenadas lst f (+ x 1) 1))))
 
-(define diccionario (coordenadas (apply append carrusel) '() 1 1))
+(define (diccionario) (coordenadas (apply append (carrusel)) '() 1 1))
 
 (define (buscar producto)
-  (cadar (filter (lambda (producto) (equal? producto (car diccionario))) diccionario)))
+  (cadar (filter (lambda (p) (equal? producto (car p))) (diccionario))))
 
-(define valor-total (apply + (map (lambda (x) (* (cadr x) (caddr x))) (apply append carrusel))))
+(define (detalles x y lst)
+  (define (elemento y lst)
+    (if (not (= 1 y))
+        (elemento (- y 1) (cdr lst))
+        (car lst)))
+  (if (not (= 1 x))
+      (detalles (- x 1) y (cdr lst))
+      (elemento y (car lst))))
+
+(define (reemplazar-elem producto x y lst f)
+  (define (reemplazar-fila producto y lst f)
+    (cond
+      ((null? lst) f)
+      ((= y 1) (reemplazar-fila producto (- y 1) (cdr lst) (append (list f) producto)))
+      (else (reemplazar-fila producto (- y 1) (cdr lst) (append (list f) (car lst))))))
+  (cond
+    ((null? lst) f)
+    ((= 1 x) (reemplazar-elem producto (- x 1) y (cdr lst) (append (list f) (reemplazar-fila producto y (car lst) '()))))
+    (else (reemplazar-elem producto (- x 1) y (cdr lst) (append (list f) (car lst))))))
+
+(define valor-total (apply + (map (lambda (x) (* (cadr x) (caddr x))) (apply append (carrusel)))))
 
 (define (productos-bajos-aux lst f)
   (if (null? lst) f
       (productos-bajos-aux (cdr lst) (append f (list (list (caar lst) (cadar lst) (buscar (caar lst))))))))
-(define productos-bajos (productos-bajos-aux (filter (lambda (x) (> 20 (cadr x))) (apply append carrusel)) '()))
+(define productos-bajos (productos-bajos-aux (filter (lambda (x) (> 20 (cadr x))) (apply append (carrusel))) '()))
 
-(define a (print "hola"))
+(define (escribir lst)
+  (define output (open-output-file "carrusel.txt" #:exists 'replace))
+  (write lst output)
+  (close-output-port output))
 
-(define (realizar-transacciones)
-  (define transaccion (read transacciones))
-  (if (eof-object? transaccion)
-      (void)
+(define (a)
+  (define coords (buscar (est-actual)))
+  (define lista (carrusel))
+  (if (= 0 (- (car coords) 1))
       (begin
-        transaccion
-        (realizar-transacciones))))
+        (displayln (detalles filas (cdr coords) lista))
+        (escribir (list lista (car (detalles filas (cdr coords) lista)))))
+      (begin
+        (displayln (detalles (- (car coords) 1) (cdr coords) lista))
+        (escribir (list (carrusel) (car (detalles (- (car coords) 1) (cdr coords) lista)))))))
 
-(realizar-transacciones)
+(define (b)
+  (define coords (buscar (est-actual)))
+  (define lista (carrusel))
+  (if (= filas (+ (car coords) 1))
+      (begin
+        (displayln (detalles 1 (cdr coords) lista))
+        (escribir (list lista (car (detalles 1 (cdr coords) lista)))))
+      (begin
+        (displayln (detalles (+ (car coords) 1) (cdr coords) lista))
+        (escribir (list lista (car (detalles (+ (car coords) 1) (cdr coords) lista)))))))
 
-"Valor total del inventario:" valor-total
-"Productos con poco o nulo inventario:" productos-bajos
+(define (c)
+  (define coords (buscar (est-actual)))
+  (define lista (carrusel))
+  (if (= 0 (- (cdr coords) 1))
+      (begin
+        (displayln "Movimiento inválido.")
+        (escribir (list lista (car (detalles (car coords) (cdr coords) lista)))))
+      (begin
+        (displayln (detalles (car coords) (- (cdr coords) 1) lista))
+        (escribir (list lista (car (detalles (car coords) (- (cdr coords) 1) lista)))))))
+
+(define (d)
+  (define coords (buscar (est-actual)))
+  (define lista (carrusel))
+  (if (< columnas (+ (cdr coords) 1))
+      (begin
+        (displayln "Movimiento inválido.")
+        (escribir (list lista (car (detalles (car coords) (cdr coords) lista)))))
+      (begin
+        (displayln (detalles (car coords) (+ (cdr coords) 1) lista))
+        (escribir (list lista (car (detalles (car coords) (+ (cdr coords) 1) lista)))))))
+
+(define (retirar cantidad)
+  (define coords (buscar (est-actual)))
+  (define lista (carrusel))
+  (define producto (detalles (car coords) (cdr coords) lista))
+  (if (< (- (cadr producto) cantidad) 0)
+      (begin
+        (displayln "Se intentó retirar más de la cantidad existente.")
+        (displayln (list (reemplazar-elem (list (car producto) 0 (caddr producto)) (car coords) (cdr coords) lista '()) (car producto))))
+      (void)))
+
+(define (realizar-transacciones arch)
+  (load arch))
+
+(realizar-transacciones "transacciones.txt")
+
+(display "Valor total del inventario: ") (displayln valor-total)
+(display "Productos con poco o nulo inventario: ") (displayln productos-bajos)
